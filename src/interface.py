a8a5c51d4fdeab441078a427e8599abb616bd22f
wyh <yihangw@opera.com>
2021-01-18 14:42:42 +0800

    1.增加参数名，修复python3.8运行pymysql报错的问题

--- src/interface.py
+++ src/interface.py
@@ -6,7 +6,21 @@ class MYSQL():
     connect=None
 
     def _Connect(self,):
-        self.connect = pymysql.connect("localhost", "root", "123456", "spider")
+        self.connect = pymysql.connect(host="localhost", user="root", password="123456", database="spider")
+
+    # def __init__(self, host=None, user=None, password="",
+    #              database=None, port=0, unix_socket=None,
+    #              charset='', sql_mode=None,
+    #              read_default_file=None, conv=None, use_unicode=None,
+    #              client_flag=0, cursorclass=Cursor, init_command=None,
+    #              connect_timeout=10, ssl=None, read_default_group=None,
+    #              compress=None, named_pipe=None,
+    #              autocommit=False, db=None, passwd=None, local_infile=False,
+    #              max_allowed_packet=16 * 1024 * 1024, defer_connect=False,
+    #              auth_plugin_map=None, read_timeout=None, write_timeout=None,
+    #              bind_address=None, binary_prefix=False, program_name=None,
+    #              server_public_key=None):
+
 
     def _CheckStatus(func):
         @wraps(func)
